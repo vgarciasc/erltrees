df2720dee7fa991777139417bf62e2eb553230f3
vgarcia <13024544+vgarciasc@users.noreply.github.com>
2023-09-18 16:59:07 -0300

    adding leaf clustering experiment

---
+++ erltrees/experiments/leaf_clustering.py
@@ -0,0 +1,134 @@
+import numpy as np
+
+from erltrees.evo.evo_tree import Individual
+from erltrees.rl.configs import get_config
+from erltrees.viz.graph_utils import convert_tree_to_tree_string, convert_tree_string_to_dot, \
+    convert_tree_to_tree_string_with_communities, COLORS
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import networkx as nx
+import graphviz
+from collections import Counter
+
+if __name__ == "__main__":
+    config = get_config("lunar_lander")
+
+    tree_str = "\n- Leg 1 is Touching <= 0.50000\n-- Y Velocity <= -0.09085\n--- Angle <= -0.04364\n---- Y Velocity <= -0.25810\n----- Y Position <= 0.20415\n------ MAIN ENGINE\n------ Angular Velocity <= -0.18925\n------- LEFT ENGINE\n------- X Velocity <= -0.17510\n-------- MAIN ENGINE\n-------- Angular Velocity <= -0.02175\n--------- LEFT ENGINE\n--------- MAIN ENGINE\n----- X Velocity <= 0.02710\n------ RIGHT ENGINE\n------ LEFT ENGINE\n---- Y Velocity <= -0.28725\n----- X Velocity <= -0.39615\n------ RIGHT ENGINE\n------ MAIN ENGINE\n----- Angle <= 0.21595\n------ X Position <= -0.02269\n------- RIGHT ENGINE\n------- Angular Velocity <= 0.18515\n-------- LEFT ENGINE\n-------- MAIN ENGINE\n------ RIGHT ENGINE\n--- Y Position <= 0.00074\n---- NOP\n---- Angle <= 0.02441\n----- LEFT ENGINE\n----- RIGHT ENGINE\n-- Y Velocity <= -0.06200\n--- MAIN ENGINE\n--- Angle <= -0.21080\n---- LEFT ENGINE\n---- NOP"
+    agent = Individual.read_from_string(config, tree_str)
+
+    leaves = agent.get_node_list(get_leaves=True)
+    cooccurrences = Counter()
+
+    leaf_history = []
+
+    env = config["maker"]()
+    for episode in range(1000):
+        done = False
+        state = env.reset()
+        curr_leaf = agent.get_leaf(state)
+        leaf_history.append([])
+
+        while not done:
+            # env.render()
+            action = agent.act(state)
+            state, reward, done, info = env.step(action)
+
+            next_leaf = agent.get_leaf(state)
+            cooccurrences[(curr_leaf, next_leaf)] += 1
+            leaf_history[-1].append(curr_leaf)
+
+            curr_leaf = next_leaf
+
+    env.close()
+
+    for (leaf1, leaf2), count in cooccurrences.items():
+        print(f"{leaves.index(leaf1)} -> {leaves.index(leaf2)}: {count}")
+
+    # create co-occurrence network
+    G = nx.Graph()
+    for (leaf1, leaf2), count in cooccurrences.items():
+        G.add_edge(f"{leaves.index(leaf1)}",
+                   f"{leaves.index(leaf2)}",
+                   weight=count)
+
+    # run louvain community detection
+    louvain_communities = nx.algorithms.community.louvain_communities(G, weight="weight")
+    greedy_modularity_communities = nx.algorithms.community.greedy_modularity_communities(G, weight="weight")
+    communities = greedy_modularity_communities
+
+    tree_string = convert_tree_to_tree_string_with_communities(config, agent, louvain_communities)
+    dot_string = convert_tree_string_to_dot(tree_string)
+    graph = graphviz.Source(dot_string, format="png")
+    graph.render(filename="tmp_louvain", cleanup=True)
+    graph.view()
+
+    tree_string = convert_tree_to_tree_string_with_communities(config, agent, greedy_modularity_communities)
+    dot_string = convert_tree_string_to_dot(tree_string)
+    graph = graphviz.Source(dot_string, format="png")
+    graph.render(filename="tmp_greedy", cleanup=True)
+    graph.view()
+
+    get_community = lambda leaf_id : [i for (i, c) in enumerate(communities) if str(leaf_id) in c]
+
+    # plot last leaf history
+    plt.subplots(2, 1, figsize=(12, 5))
+    node_ids = [[leaves.index(leaf) for leaf in history] for history in leaf_history]
+    comm_ids = [[get_community(leaf_id)[0] if len(get_community(leaf_id)) > 0 else -1 for leaf_id in history] for history in node_ids]
+    x = [[i for i in range(len(history))] for history in node_ids]
+    x_percentage = [[i / len(history) for i in range(len(history))] for history in node_ids]
+    episode_lengths = [len(history) for history in leaf_history]
+    max_episode_length = max(episode_lengths)
+    ax = plt.subplot(2, 1, 1)
+    ax.set_xlabel("Episode Progress")
+    ax.set_ylabel("Leaf ID")
+    sns.scatterplot(x=x_percentage[-1],
+                    y=node_ids[-1],
+                    hue=comm_ids[-1],
+                    palette=COLORS,
+                    edgecolor='none', alpha=0.5,
+                    legend=False, ax=ax)
+    ax = plt.subplot(2, 1, 2)
+    ax.set_xlabel("Episode Progress")
+    ax.set_ylabel("Leaf group")
+    sns.scatterplot(x=x_percentage[-1],
+                    y=comm_ids[-1],
+                    hue=comm_ids[-1],
+                    palette=COLORS,
+                    edgecolor='none', alpha=0.5,
+                    legend=False, ax=ax)
+    plt.suptitle("Last Episode", fontweight='bold', fontsize=16)
+    plt.tight_layout()
+    plt.show()
+
+    # plot last leaf history
+    plt.subplots(2, 1, figsize=(12, 5))
+    node_ids = [[leaves.index(leaf) for leaf in history] for history in leaf_history]
+    comm_ids = [[get_community(leaf_id)[0] if len(get_community(leaf_id)) > 0 else -1 for leaf_id in history] for history in node_ids]
+    x = [[i for i in range(len(history))] for history in node_ids]
+    x_percentage = [[i / len(history) for i in range(len(history))] for history in node_ids]
+    episode_lengths = [len(history) for history in leaf_history]
+    max_episode_length = max(episode_lengths)
+    ax = plt.subplot(2, 1, 1)
+    ax.set_xlabel("Episode Progress")
+    ax.set_ylabel("Leaf ID")
+    sns.scatterplot(x=np.concatenate(x_percentage),
+                    y=np.concatenate(node_ids),
+                    hue=np.concatenate(comm_ids),
+                    palette=COLORS,
+                    edgecolor='none', alpha=0.1,
+                    legend=False, ax=ax)
+    ax = plt.subplot(2, 1, 2)
+    ax.set_xlabel("Episode Progress")
+    ax.set_ylabel("Leaf group")
+    sns.scatterplot(x=np.concatenate(x_percentage),
+                    y=np.concatenate(comm_ids),
+                    hue=np.concatenate(comm_ids),
+                    palette=COLORS,
+                    edgecolor='none', alpha=0.1,
+                    legend=False, ax=ax)
+    plt.suptitle("All Episodes", fontweight='bold', fontsize=16)
+    plt.tight_layout()
+    plt.show()
+
--- erltrees/viz/graph_utils.py
+++ erltrees/viz/graph_utils.py
@@ -4,6 +4,8 @@ import graphviz
 import io
 from erltrees.evo.evo_tree import Individual
 
+COLORS = ["crimson", "blue", "green", "gold", "lightseagreen", "lightpink", "lightsalmon", "khaki", "indigo", "hotpink", "lightgoldenrod", "purple", "maroon"]
+
 def convert_tree_string_to_dot(string):
     dot_string = "graph DecisionTree {"
     lines = [line.strip() for line in string.split("\n")]
@@ -20,10 +22,14 @@ def convert_tree_string_to_dot(string):
 
         node_color = "grey95" if "<=" in node_label else "grey99"
         edge_color = "black"
+
         if "*" in node_label:
-            # edge_color = "lightcoral"
             node_color = "lightcoral"
             node_label = node_label[:-2] # remove " *"
+
+        if "comm" in node_label:
+            node_color = COLORS[int(node_label[-1]) - 1] if node_label[-1] != "X" else "white"
+            node_label = node_label[:-5] # remove "commX"
 
         dot_string += f'{"  " * depth}{node_id}[label=\"{node_label}\", shape=box, style=filled, fillcolor={node_color}, color={edge_color}];\n'
 
@@ -41,8 +47,10 @@ def make_image_from_dot(dot_string):
     image_bytes = graph.pipe(format='png')
     return Image.open(io.BytesIO(image_bytes))
 
-def convert_tree_to_tree_string(config, tree, state):
-    stack = [(tree, 1, True)]
+def convert_tree_to_tree_string(config, tree, state=None):
+    should_color_nodes = (state is not None)
+
+    stack = [(tree, 1, should_color_nodes)]
     output = ""
 
     while len(stack) > 0:
@@ -60,12 +68,43 @@ def convert_tree_to_tree_string(config, tree, state):
             output += '{:.5f}'.format(node.threshold)
 
             if node.right:
-                stack.append((node.right, depth + 1, in_path and state[node.attribute] > node.threshold))
+                stack.append((node.right, depth + 1, should_color_nodes and in_path and state[node.attribute] > node.threshold))
             if node.left:
-                stack.append((node.left, depth + 1, in_path and state[node.attribute] <= node.threshold))
+                stack.append((node.left, depth + 1, should_color_nodes and in_path and state[node.attribute] <= node.threshold))
 
         if in_path:
             output += " *"
+        output += "\n"
+
+    return output
+def convert_tree_to_tree_string_with_communities(config, tree, communities=None):
+    stack = [(tree, 1)]
+    output = ""
+
+    while len(stack) > 0:
+        node, depth = stack.pop()
+        output += "-" * depth + " "
+
+        if node.is_leaf():
+            if config["action_type"] == "continuous":
+                output += str(node.label)
+            else:
+                output += (config['actions'][node.label]).upper()
+        else:
+            output += config['attributes'][node.attribute][0]
+            output += " <= "
+            output += '{:.5f}'.format(node.threshold)
+
+            if node.right:
+                stack.append((node.right, depth + 1))
+            if node.left:
+                stack.append((node.left, depth + 1))
+
+        if node.is_leaf() and communities is not None:
+            leaf_id = tree.get_node_list(get_leaves=True).index(node)
+            group_id = [i for (i, c) in enumerate(communities) if str(leaf_id) in c]
+            group_id = group_id[0] if len(group_id) > 0 else 'X'
+            output += f" comm{group_id}"
         output += "\n"
 
     return output
